01dd5a7c68de7f510ac8051d62898943778facc6
QuAzZeR <natsamiss@gmail.com>
2017-03-20 01:28:31 +0700

    can get sequence of text in book

--- preprocess.py
+++ preprocess.py
@@ -1,28 +1,14 @@
-
-
-
-def get_thai_list():
-    filesname = []
-    lines = []
-    with open('../Data/article/list_name.txt','r')as list_filename:
-        filesname = [line.strip() for line in list_filename.readlines()]
-    #print (filesname)
-    for filename in filesname[:20]:
-        with open('../Data/article/'+filename,'r') as article_file:
-            lines += [line.strip() for line in article_file.readlines()]
-        print("finish read file" + filename)
-        
+from tokenize import word_tokenize
+FILE_PATH = '../data/Dek-d/RealFace/%d'
+RANGE = range(1,16)
+def get_seq_of_word():
     sequence_word = []
-    for line in lines:
-        line_split = line.split('|')
-        
-        sequence_word += line_split[:len(line_split)-1]
-        sequence_word.append(' ')
-        # print (sequence_word)
+    for i in RANGE:
+        with open(FILE_PATH%(i),'r') as text_file:
+            lines = text_file.read()
+            sequence_word += word_tokenize(lines)
     return sequence_word
-        
-
 
 
 if __name__ == '__main__':
-    get_thai_list()
+    print(len(get_seq_of_word()))
